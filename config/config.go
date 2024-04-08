299ddb2956420c6f13a3168da638fea64b225643
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-08 10:51:36 +0700

    feat: add config loader

---
+++ config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	ErrMissingKey = errors.New("missing key")
+)
+
+type Config struct {
+	DatabaseURL string
+	JWTIssuer   string
+	JWTSecret   string
+	JWTLifespan time.Duration
+}
+
+func InitConfig() error {
+	return godotenv.Load(".env")
+}
+
+func LoadConfig() (Config, error) {
+	env, err := godotenv.Read(".env")
+	if err != nil {
+		return Config{}, err
+	}
+
+	ret := Config{}
+
+	s, ok := env["DATABASE_URL"]
+	if !ok {
+		return Config{}, ErrMissingKey
+	}
+	ret.DatabaseURL = s
+
+	s, ok = env["JWT_ISSUER"]
+	if !ok {
+		return Config{}, ErrMissingKey
+	}
+	ret.JWTIssuer = s
+
+	s, ok = env["JWT_SECRET"]
+	if !ok {
+		return Config{}, ErrMissingKey
+	}
+	ret.JWTSecret = s
+
+	s, ok = env["JWT_LIFESPAN"]
+	if !ok {
+		return Config{}, ErrMissingKey
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return Config{}, err
+	}
+	ret.JWTLifespan = time.Duration(i) * time.Minute
+
+	return ret, nil
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.3.1
+	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.8.3
 	golang.org/x/crypto v0.17.0
