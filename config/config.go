cb2abab60d1476610727a3ebb1885ae2f841e123
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-09 10:48:16 +0700

    feat: add google api and session key environment variable

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"encoding/base64"
 	"errors"
+	"log"
 	"os"
 	"strconv"
 	"time"
@@ -16,9 +18,16 @@ var (
 type Config struct {
 	ServerAddr  string
 	DatabaseURL string
+
+	SessionKey []byte
+
 	JWTIssuer   string
 	JWTSecret   string
 	JWTLifespan time.Duration
+
+	GoogleAPIClientID     string
+	GoogleAPIClientSecret string
+	GoogleAPIRedirectURL  string
 }
 
 func InitConfig() error {
@@ -32,16 +41,28 @@ func LoadConfig() (Config, error) {
 
 	ret.DatabaseURL = os.Getenv("DATABASE_URL")
 
+	s := os.Getenv("SESSION_KEY")
+	log.Println(s)
+	b, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return Config{}, err
+	}
+	ret.SessionKey = b
+
 	ret.JWTIssuer = os.Getenv("JWT_ISSUER")
 
 	ret.JWTSecret = os.Getenv("JWT_SECRET")
 
-	s := os.Getenv("JWT_LIFESPAN")
+	s = os.Getenv("JWT_LIFESPAN")
 	i, err := strconv.Atoi(s)
 	if err != nil {
 		return Config{}, err
 	}
 	ret.JWTLifespan = time.Duration(i) * time.Minute
+
+	ret.GoogleAPIClientID = os.Getenv("GOOGLE_API_CLIENT_ID")
+	ret.GoogleAPIClientSecret = os.Getenv("GOOGLE_API_CLIENT_SECRET")
+	ret.GoogleAPIRedirectURL = os.Getenv("GOOGLE_API_REDIRECT_URL")
 
 	return ret, nil
 }
