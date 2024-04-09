27e4bf22ec22c0709fd7864230114d261a2a7a1b
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-09 14:42:14 +0700

    feat: add account domain

---
+++ domain/account.go
@@ -0,0 +1,53 @@
+package domain
+
+import "context"
+
+const (
+	AccountTypeRegular = "regular"
+	AccountTypeGoogle  = "google"
+
+	AccountRoleAdmin           = "admin"
+	AccountRoleUser            = "user"
+	AccountRoleDoctor          = "doctor"
+	AccountRolePharmacyManager = "pharmacy_manager"
+)
+
+type Account struct {
+	ID            int64
+	Email         string
+	EmailVerified bool
+	Role          string
+	AccountType   string
+}
+
+type AccountWithCredentials struct {
+	Account        Account
+	HashedPassword string
+}
+
+type AccountLoginCredentials struct {
+	Email    string
+	Password string
+}
+
+type AccountRegisterCredentials struct {
+	Email string
+}
+
+type AccountRepository interface {
+	GetByEmail(ctx context.Context, email string) (Account, error)
+	GetWithCredentialsByEmail(ctx context.Context, email string) (AccountWithCredentials, error)
+	IsExistByEmail(ctx context.Context, email string) (bool, error)
+
+	GetByID(ctx context.Context, id int64) (Account, error)
+	GetWithCredentialsByID(ctx context.Context, id int64) (AccountWithCredentials, error)
+	IsExistByID(ctx context.Context, id int64) (bool, error)
+
+	Add(ctx context.Context, creds AccountWithCredentials) (Account, error)
+	UpdateByPassword(ctx context.Context, id int64, newHashedPassword string) error
+}
+
+type AccountService interface {
+	Register(ctx context.Context, creds AccountRegisterCredentials) (Account, error)
+	Login(ctx context.Context, creds AccountLoginCredentials) (AuthTokens, error)
+}
