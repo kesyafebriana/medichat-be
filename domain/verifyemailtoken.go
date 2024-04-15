74128a7efbc5a6d79acaec47bfaa18fb625e0fff
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-15 15:19:26 +0700

    feat(vet): add verify email token entity and repository interface

---
+++ domain/verifyemailtoken.go
@@ -0,0 +1,21 @@
+package domain
+
+import (
+	"context"
+	"time"
+)
+
+type VerifyEmailToken struct {
+	ID        int64
+	Account   Account
+	Token     string
+	ExpiredAt time.Time
+}
+
+type VerifyEmailTokenRepository interface {
+	Add(ctx context.Context, token VerifyEmailToken) (ResetPasswordToken, error)
+	GetByID(ctx context.Context, id int64) (VerifyEmailToken, error)
+	GetByTokenStr(ctx context.Context, tokenStr string) (VerifyEmailToken, error)
+	GetByTokenStrAndLock(ctx context.Context, tokenStr string) (VerifyEmailToken, error)
+	SoftDeleteByID(ctx context.Context, id int64) error
+}
