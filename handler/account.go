e818d40693413c257155921a481577e9c322700d
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-15 14:11:04 +0700

    feat(account): add handler

---
+++ handler/account.go
@@ -0,0 +1,122 @@
+package handler
+
+import (
+	"medichat-be/apperror"
+	"medichat-be/domain"
+	"medichat-be/dto"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AccountHandler struct {
+	accountSrv domain.AccountService
+}
+
+type AccountHandlerOpts struct {
+	AccountSrv domain.AccountService
+}
+
+func NewAccountHandler(opts AccountHandlerOpts) *AccountHandler {
+	return &AccountHandler{
+		accountSrv: opts.AccountSrv,
+	}
+}
+
+func (h *AccountHandler) Register(ctx *gin.Context) {
+	var req dto.AccountRegisterRequest
+
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	creds := req.ToCredentials()
+
+	_, err = h.accountSrv.Register(ctx, creds)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusCreated,
+		dto.ResponseCreated(nil),
+	)
+}
+
+func (h *AccountHandler) Login(ctx *gin.Context) {
+	var req dto.AccountLoginRequest
+
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	creds := req.ToCredentials()
+
+	token, err := h.accountSrv.Login(ctx, creds)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusOK,
+		dto.ResponseOk(token),
+	)
+}
+
+func (h *AccountHandler) ForgetPassword(ctx *gin.Context) {
+	var req dto.AccountForgetPasswordRequest
+
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	token, err := h.accountSrv.GetResetPasswordToken(ctx, req.Email)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusCreated,
+		dto.ResponseCreated(token),
+	)
+}
+
+func (h *AccountHandler) ResetPassword(ctx *gin.Context) {
+	var req dto.AccountResetPasswordRequest
+
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	creds := req.ToCredentials()
+
+	err = h.accountSrv.ResetPassword(ctx, creds)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusCreated,
+		dto.ResponseCreated(nil),
+	)
+}
