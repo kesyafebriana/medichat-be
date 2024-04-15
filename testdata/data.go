6d5f8f78185fe555c605f5cc926532ac945041d5
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-15 17:00:33 +0700

    test(data): add repository mock utilities

---
+++ testdata/data.go
@@ -0,0 +1,25 @@
+package testdata
+
+import (
+	"medichat-be/domain"
+	"medichat-be/mocks/domainmocks"
+)
+
+type DataRepositoryMockOpts struct {
+	AccountRepository            domain.AccountRepository
+	ResetPasswordTokenRepository domain.ResetPasswordTokenRepository
+	VerifyEmailTokenRepository   domain.VerifyEmailTokenRepository
+}
+
+func NewDataRepositoryMock(opts DataRepositoryMockOpts) *domainmocks.DataRepository {
+	dataRepo := new(domainmocks.DataRepository)
+
+	dataRepo.On("AccountRepository").
+		Return(opts.AccountRepository)
+	dataRepo.On("ResetPasswordTokenRepository").
+		Return(opts.ResetPasswordTokenRepository)
+	dataRepo.On("VerifyEmailTokenRepository").
+		Return(opts.VerifyEmailTokenRepository)
+
+	return dataRepo
+}
