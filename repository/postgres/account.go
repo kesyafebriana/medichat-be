1948a6cc0bfe84001ab2d7d99ba680c4ea7aec38
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-19 13:42:17 +0700

    refactor(account): reorganize util functions

--- repository/postgres/account.go
+++ repository/postgres/account.go
@@ -2,7 +2,6 @@ package postgres
 
 import (
 	"context"
-	"database/sql"
 	"medichat-be/domain"
 )
 
@@ -212,27 +211,4 @@ func (r *accountRepository) VerifyEmailByID(
 		r.querier, ctx, q,
 		id,
 	)
-}
-
-var (
-	accountColumns                = " id, email, email_verified, role, account_type "
-	accountWithCredentialsColumns = " id, email, email_verified, role, account_type, hashed_password "
-)
-
-func accountScanDests(u *domain.Account) []any {
-	return []any{
-		&u.ID, &u.Email, &u.EmailVerified, &u.Role, &u.AccountType,
-	}
-}
-
-func scanAccountWithCredentials(r RowScanner, a *domain.AccountWithCredentials) error {
-	var nullHashedPassword sql.NullString
-	if err := r.Scan(
-		&a.Account.ID, &a.Account.Email, &a.Account.EmailVerified,
-		&a.Account.Role, &a.Account.AccountType, &nullHashedPassword,
-	); err != nil {
-		return err
-	}
-	a.HashedPassword = toStringPtr(nullHashedPassword)
-	return nil
 }
