f0a0864ef4d02d61932b99b304aff247a5b6a05a
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-19 16:11:41 +0700

    feat: add repository interfaces for profiles

--- domain/doctor.go
+++ domain/doctor.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Doctor struct {
 	ID             int64
@@ -13,4 +16,15 @@ type Doctor struct {
 	IsActive      bool
 	StartWorkDate time.Time
 	Price         int64
+}
+
+type DoctorRepository interface {
+	GetByID(ctx context.Context, id int64) (Doctor, error)
+	GetByIDAndLock(ctx context.Context, id int64) (Doctor, error)
+
+	GetByAccountID(ctx context.Context, id int64) (Doctor, error)
+	GetByAccountIDAndLock(ctx context.Context, id int64) (Doctor, error)
+
+	Add(ctx context.Context, d Doctor) (Doctor, error)
+	Update(ctx context.Context, d Doctor) (Doctor, error)
 }
--- domain/user.go
+++ domain/user.go
@@ -1,7 +1,20 @@
 package domain
 
+import "context"
+
 type User struct {
 	ID int64
 
 	DateOfBirth int64
+}
+
+type UserRepository interface {
+	GetByID(ctx context.Context, id int64) (User, error)
+	GetByIDAndLock(ctx context.Context, id int64) (User, error)
+
+	GetByAccountID(ctx context.Context, id int64) (User, error)
+	GetByAccountIDAndLock(ctx context.Context, id int64) (User, error)
+
+	Add(ctx context.Context, u User) (User, error)
+	Update(ctx context.Context, u User) (User, error)
 }
