3443f3f8a141a4000eae5afc2c53cfe9e78e3255
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-04-23 14:40:30 +0700

    feat(user): update domain

--- domain/user.go
+++ domain/user.go
@@ -1,21 +1,40 @@
 package domain
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type User struct {
 	ID      int64
 	Account Account
 
-	DateOfBirth int64
+	Name        string
+	PhotoURL    string
+	DateOfBirth time.Time
+}
+
+type UserUpdateDetails struct {
+	ID          int64
+	Name        *string
+	PhotoURL    *string
+	DateOfBirth *time.Time
 }
 
 type UserRepository interface {
 	GetByID(ctx context.Context, id int64) (User, error)
 	GetByIDAndLock(ctx context.Context, id int64) (User, error)
+	IsExistByID(ctx context.Context, id int64) (bool, error)
 
 	GetByAccountID(ctx context.Context, id int64) (User, error)
 	GetByAccountIDAndLock(ctx context.Context, id int64) (User, error)
+	IsExistByAccountID(ctx context.Context, id int64) (bool, error)
 
 	Add(ctx context.Context, u User) (User, error)
 	Update(ctx context.Context, u User) (User, error)
+}
+
+type UserService interface {
+	CreateProfile(ctx context.Context, u User) (User, error)
+	UpdateProfile(ctx context.Context, u UserUpdateDetails) (User, error)
 }
