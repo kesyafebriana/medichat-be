0e9422aed669153f0a821959ae26e638852d99c8
Roihan <hroihan6@gmail.com>
2024-04-29 11:28:37 +0700

    update category handler due to add photo_url attribute

--- handler/category.go
+++ handler/category.go
@@ -2,9 +2,12 @@ package handler
 
 import (
 	"medichat-be/apperror"
+	"medichat-be/constants"
 	"medichat-be/domain"
 	"medichat-be/dto"
+	"mime/multipart"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -80,18 +83,48 @@ func (h *CategoryHandler) GetCategoryBySlug(ctx *gin.Context) {
 }
 
 func (h *CategoryHandler) CreateCategoryLevelOne(ctx *gin.Context) {
-	var req dto.CreateCategoryRequest
-
-	err := ctx.ShouldBindJSON(&req)
+	var form dto.CreateCategoryForm
+	contentLength := ctx.Request.Header.Get("Content-Length")
+	i, err := strconv.Atoi(contentLength)
 	if err != nil {
+		ctx.Error(apperror.NewInternal(err))
+		ctx.Abort()
+		return
+	}
+
+	if i > constants.MaxImageSize {
+		ctx.Error(apperror.NewImageSizeExceeded("500kb"))
+		ctx.Abort()
+		return
+	}
+
+	if err := ctx.ShouldBind(&form); err != nil {
 		ctx.Error(apperror.NewBadRequest(err))
 		ctx.Abort()
 		return
 	}
 
+	var file *multipart.File
+	if form.Image != nil {
+		fileType := form.Image.Header.Get("Content-Type")
+		if fileType != constants.ImageJpeg && fileType != constants.ImageJpg && fileType != constants.ImagePng {
+			ctx.Error(apperror.NewRestrictredFileType(constants.ImageJpeg, constants.ImageJpg, constants.ImagePng))
+			ctx.Abort()
+			return
+		}
+		f, err := form.Image.Open()
+		if err != nil {
+			ctx.Error(apperror.NewInternal(err))
+			ctx.Abort()
+			return
+		}
+		file = &f
+		defer f.Close()
+	}
+
 	category, err := h.categorySrv.CreateCategoryLevelOne(ctx, domain.Category{
-		Name: req.Name,
-	})
+		Name: form.Name,
+	}, file)
 
 	if err != nil {
 		ctx.Error(err)
@@ -153,28 +186,60 @@ func (h *CategoryHandler) DeleteCategory(ctx *gin.Context) {
 }
 
 func (h *CategoryHandler) UpdateCategory(ctx *gin.Context) {
-	var req dto.UpdateCategoryRequest
+	var form dto.UpdateCategoryForm
+	contentLength := ctx.Request.Header.Get("Content-Length")
 	var params dto.CategorySlugParams
 
-	err := ctx.ShouldBindJSON(&req)
+	err := ctx.ShouldBindUri(&params)
 	if err != nil {
 		ctx.Error(apperror.NewBadRequest(err))
 		ctx.Abort()
 		return
 	}
 
-	err = ctx.ShouldBindUri(&params)
+	i, err := strconv.Atoi(contentLength)
+	if err != nil {
+		ctx.Error(apperror.NewInternal(err))
+		ctx.Abort()
+		return
+	}
+
+	if i > constants.MaxImageSize {
+		ctx.Error(apperror.NewImageSizeExceeded("500kb"))
+		ctx.Abort()
+		return
+	}
+
+	err = ctx.ShouldBind(&form)
 	if err != nil {
 		ctx.Error(apperror.NewBadRequest(err))
 		ctx.Abort()
 		return
+	}
+
+	var file *multipart.File
+	if form.Image != nil {
+		fileType := form.Image.Header.Get("Content-Type")
+		if fileType != constants.ImageJpeg && fileType != constants.ImageJpg && fileType != constants.ImagePng {
+			ctx.Error(apperror.NewRestrictredFileType(constants.ImageJpeg, constants.ImageJpg, constants.ImagePng))
+			ctx.Abort()
+			return
+		}
+		f, err := form.Image.Open()
+		if err != nil {
+			ctx.Error(apperror.NewInternal(err))
+			ctx.Abort()
+			return
+		}
+		file = &f
+		defer f.Close()
 	}
 
 	category, err := h.categorySrv.UpdateCategory(ctx, domain.Category{
-		Name:     req.Name,
-		ParentID: req.ParentId,
+		Name:     form.Name,
+		ParentID: form.ParentId,
 		Slug:     params.Slug,
-	})
+	}, file)
 	if err != nil {
 		ctx.Error(err)
 		ctx.Abort()
