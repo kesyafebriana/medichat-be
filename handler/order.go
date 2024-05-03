1556ddc568f8fd41451fc10d741615a8e478d79b
Muhammad Naufal Fakhrizal <muhammad.fakhrizal@shopee.com>
2024-05-03 09:45:00 +0700

    feat(order): implement handlers

--- handler/order.go
+++ handler/order.go
@@ -1,7 +1,11 @@
 package handler
 
 import (
+	"medichat-be/apperror"
 	"medichat-be/domain"
+	"medichat-be/dto"
+	"medichat-be/util"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,29 +25,171 @@ func NewOrderHandler(opts OrderHandlerOpts) *OrderHandler {
 }
 
 func (h *OrderHandler) ListOrders(ctx *gin.Context) {
+	var q dto.OrderListQuery
 
+	err := ctx.ShouldBindQuery(&q)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	det := q.ToDetails()
+
+	orders, page, err := h.orderSrv.List(ctx, det)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusOK,
+		dto.ResponseOk(map[string]any{
+			"page_info": dto.NewPageInfoResponse(page),
+			"orders":    util.MapSlice(orders, dto.NewOrderResponse),
+		}),
+	)
 }
 
 func (h *OrderHandler) GetOrderByID(ctx *gin.Context) {
+	var uri dto.IDPathRequest
 
+	err := ctx.ShouldBindUri(&uri)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	order, err := h.orderSrv.GetByID(ctx, uri.ID)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusOK,
+		dto.ResponseOk(dto.NewOrderResponse(order)),
+	)
 }
 
 func (h *OrderHandler) GetCartInfo(ctx *gin.Context) {
+	var req dto.OrdersCreateRequest
 
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	dets := req.ToDetails()
+
+	orders, err := h.orderSrv.GetCartInfo(ctx, dets)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusOK,
+		dto.ResponseOk(dto.NewOrdersResponse(orders)),
+	)
 }
 
-func (h *OrderHandler) AddOrder(ctx *gin.Context) {
+func (h *OrderHandler) AddOrders(ctx *gin.Context) {
+	var req dto.OrdersCreateRequest
 
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	dets := req.ToDetails()
+
+	orders, err := h.orderSrv.AddOrders(ctx, dets)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusCreated,
+		dto.ResponseCreated(dto.NewOrdersResponse(orders)),
+	)
 }
 
 func (h *OrderHandler) SendOrder(ctx *gin.Context) {
+	var uri dto.IDPathRequest
 
+	err := ctx.ShouldBindUri(&uri)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	err = h.orderSrv.SendOrder(ctx, uri.ID)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusCreated,
+		dto.ResponseOk(nil),
+	)
 }
 
 func (h *OrderHandler) FinishOrder(ctx *gin.Context) {
+	var uri dto.IDPathRequest
 
+	err := ctx.ShouldBindUri(&uri)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	err = h.orderSrv.FinishOrder(ctx, uri.ID)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusCreated,
+		dto.ResponseOk(nil),
+	)
 }
 
-func (h *OrderHandler) CancelOrders(ctx *gin.Context) {
+func (h *OrderHandler) CancelOrder(ctx *gin.Context) {
+	var uri dto.IDPathRequest
 
+	err := ctx.ShouldBindUri(&uri)
+	if err != nil {
+		ctx.Error(apperror.NewBadRequest(err))
+		ctx.Abort()
+		return
+	}
+
+	err = h.orderSrv.CancelOrder(ctx, uri.ID)
+	if err != nil {
+		ctx.Error(apperror.Wrap(err))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(
+		http.StatusCreated,
+		dto.ResponseOk(nil),
+	)
 }
