09c500a5128fabe6857c845de58f581e894cbd80
Kesya Febriana Manampiring <kesya.febriana@shopee.com>
2024-05-06 17:03:29 +0700

    fix: error chat repo

--- handler/chat.go
+++ handler/chat.go
@@ -25,15 +25,15 @@ func (h *ChatHandler) Chat(ctx *gin.Context) {
 	req.Type = ctx.PostForm("type")
 	req.UserName = ctx.PostForm("userName")
 
-	timeChat,err := time.Parse("2006-01-02T15:04:05Z07:00",ctx.PostForm("createdAt"))
+	timeChat, err := time.Parse("2006-01-02T15:04:05Z07:00", ctx.PostForm("createdAt"))
 	if err != nil {
 		ctx.Error(err)
-        ctx.Abort()
-        return
+		ctx.Abort()
+		return
 	}
 	req.CreatedAt = timeChat
-	userId,err := strconv.Atoi(ctx.PostForm("userId"))
-	if err!= nil {
+	userId, err := strconv.Atoi(ctx.PostForm("userId"))
+	if err != nil {
 		ctx.Error(apperror.NewBadRequest(err))
 		ctx.Abort()
 		return
@@ -46,54 +46,52 @@ func (h *ChatHandler) Chat(ctx *gin.Context) {
 	}
 
 	// Implement Static Type !!!
-	if (req.Type == "text"){
+	if req.Type == "text" {
 		req.Message = ctx.PostForm("message")
 		if req.Message == "" {
 			ctx.Error(apperror.NewBadRequest(nil))
 			ctx.Abort()
 			return
 		}
-		err = h.chatService.PostMessage(&req,roomId,ctx)
-	} else if(req.Type == "files"){
-		fileHeader,err := ctx.FormFile("file")
+		err = h.chatService.PostMessage(&req, roomId, ctx)
+	} else if req.Type == "files" {
+		fileHeader, err := ctx.FormFile("file")
 
-		
-		if err!= nil {
+		if err != nil {
 			ctx.Error(apperror.NewBadRequest(err))
-            ctx.Abort()
-            return
-        }
-		req.File= fileHeader
+			ctx.Abort()
+			return
+		}
+		req.File = fileHeader
 
-		err = h.chatService.PostFile(&req,roomId,ctx)
-		if err!= nil {
-            ctx.Error(apperror.NewBadRequest(err))
-            ctx.Abort()
-            return
-        }
+		err = h.chatService.PostFile(&req, roomId, ctx)
+		if err != nil {
+			ctx.Error(apperror.NewBadRequest(err))
+			ctx.Abort()
+			return
+		}
 	}
 
-
-	if err!= nil {
-        ctx.Error(apperror.NewInternal(err))
-        ctx.Abort()
-        return
-    }
+	if err != nil {
+		ctx.Error(apperror.NewInternal(err))
+		ctx.Abort()
+		return
+	}
 	ctx.JSON(http.StatusOK, gin.H{"message": "message sent"})
 
 }
 
 func (h *ChatHandler) CreateRoom(ctx *gin.Context) {
 
-	doctorId,err := strconv.Atoi(ctx.PostForm("doctorId"))
-	if err != nil{
+	doctorId, err := strconv.Atoi(ctx.PostForm("doctorId"))
+	if err != nil {
 		ctx.Error(apperror.NewBadRequest(err))
-        ctx.Abort()
-        return
+		ctx.Abort()
+		return
 	}
-	
-	err = h.chatService.CreateRoom(doctorId,ctx)
-	if err!= nil {
+
+	err = h.chatService.CreateRoom(doctorId, ctx)
+	if err != nil {
 		ctx.Error(apperror.NewBadRequest(err))
 		ctx.Abort()
 		return
@@ -107,8 +105,8 @@ func (h *ChatHandler) CloseRoom(ctx *gin.Context) {
 
 	roomId := ctx.Query("roomId")
 
-	err := h.chatService.CloseRoom(roomId,ctx)
-	if err!= nil {
+	err := h.chatService.CloseRoom(roomId, ctx)
+	if err != nil {
 		ctx.Error(apperror.NewBadRequest(err))
 		ctx.Abort()
 		return
